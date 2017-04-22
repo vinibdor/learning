f4434b59b38885817a28154a3b544b9c5e6c5482
Vinícius Ramos <vinibdr@gmail.com>
2017-04-21 21:13:39 -0300

    [JSF - Relocatable Resource] Defining a composite component that uses javascript and its source is linked in the head section of the enclosing page

---
+++ Java/J2EE7_Tutorial/JSF/RelocResource/src/main/webapp/resources/js/reloc.js
@@ -0,0 +1,10 @@
+/**
+ * Created by vinib on 21/04/2017.
+ *
+ * Purpose: test the relocatable resource feature of a facelets application
+ */
+var reloc = {
+    putMeInHeadSection: function () {
+        alert("Yeah. They put me in the head section");
+    }
+}
